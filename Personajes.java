51ed354ed288e2bb19d5d52e0d63a9e79815f450
Esteban Mongui <esteband.itorres@gmail.com>
2026-04-14 20:28:59 -0600

    ejercicio 1: adivina el personaje

--- Personajes.java
+++ Personajes.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+
 public class Personajes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -30,85 +31,106 @@ public class Personajes {
 
 
     public static String decifrandoPersonaje(){
+
+        // Reemplaza las preguntas actuales por reguntas del tipo Sí/No
         System.out.println("Responde a las siguientes preguntas para descubrir el personaje que elegiste:");
-        System.out.println("¿Es un personaje real o ficticio? (real/ficticio)");
+        System.out.println("¿Es un personaje real? (Sí/No)");
         Scanner scanner = new Scanner(System.in);
         String respuesta = scanner.nextLine().toLowerCase();
         String personaje = "";
 
-        if (respuesta.equals("real")) {
-            System.out.println("¿Es un personaje histórico o contemporáneo? (histórico/contemporáneo)");
+        if (respuesta.equals("sí")) {
+            System.out.println("¿Es un personaje histórico? (sí/no)");
             respuesta = scanner.nextLine().toLowerCase();
 
-            if (respuesta.equals("histórico")) {
-                System.out.println("¿Es un personaje político, literario o artístico? (político/literario/artístico)");
+            if (respuesta.equals("sí")) {
+                System.out.println("¿Es un personaje político? (sí/no)");
                 respuesta = scanner.nextLine().toLowerCase();
 
-                if (respuesta.equals("político")) {
-                    System.out.println("¿Es Estadounidense? (si/no)");
+                if (respuesta.equals("sí")) {
+                    System.out.println("¿Es Estadounidense? (sí/no)");
                     respuesta = scanner.nextLine().toLowerCase();
-                    if (respuesta.equals("si")) {
+                    if (respuesta.equals("sí")) {
                         personaje = "Joe Biden";
                     } else if (respuesta.equals("no")) {
                         personaje = "Kim Jong Un";
                     }
-                } else if (respuesta.equals("literario")) {
-                    personaje = "José Saramago";
-                } else if (respuesta.equals("artístico")) {
-                    personaje = "Günter Grass";
+                } else if (respuesta.equals("no")) {
+                    System.out.println("¿Es un personaje literario y artístico? (sí/no)");
+                    respuesta = scanner.nextLine().toLowerCase();
+                    if (respuesta.equals("sí")) {
+                        personaje = "Günter Grass";
+                    }
+                    if (respuesta.equals("no")) {
+                        personaje = "José Saramago";
+                    }
                 } else{
                     System.out.println("Respuesta inválida.");
                 }
-            } else if (respuesta.equals("contemporáneo")) {
-                System.out.println("¿Es un personaje del deporte, música o cine? (deporte/música/cine)");
+            } else if (respuesta.equals("no")) {
+                System.out.println("¿Es un personaje del deporte? (sí/no)");
                 respuesta = scanner.nextLine().toLowerCase();
 
-                if (respuesta.equals("deporte")) {
-                    System.out.println("¿Es un futbolista, basquetbolista o boxeador? (futbolista/basquetbolista/boxeador)");
+                if (respuesta.equals("sí")) {
+                    System.out.println("¿Es un futbolista? (sí/no)");
                     respuesta = scanner.nextLine().toLowerCase();
-                    if (respuesta.equals("futbolista")) {
+                    if (respuesta.equals("sí")) {
                         personaje = "Radamel Falcao García";
-                    } else if (respuesta.equals("basquetbolista")) {
-                        personaje = "Michael Jordan";
-                    } else if (respuesta.equals("boxeador")) {
-                        personaje = "Muhammad Ali";
+                    } else if (respuesta.equals("no")) {
+                        System.out.println("¿Es un basquetbolista? (sí/no)");
+                        respuesta = scanner.nextLine().toLowerCase();
+                        if (respuesta.equals("sí")) {
+                            personaje = "Michael Jordan";
+                        } else if (respuesta.equals("boxeador")) {
+                            personaje = "Muhammad Ali";
+                        } else {
+                            System.out.println("Respuesta inválida.");
+                        }
                     }
-                } else if (respuesta.equals("música")) {
-                    personaje = "Eminem";
-                } else if (respuesta.equals("cine")) {
-                    personaje = "Adam Sandler";
-                } else {
-                    System.out.println("Respuesta inválida.");
+                } else if (respuesta.equals("no")) {
+                    System.out.println("¿Es un personaje de la música? (sí/no)");
+                    respuesta = scanner.nextLine().toLowerCase();
+                    if (respuesta.equals("sí")) {
+                        personaje = "Eminem";
+                    } else if (respuesta.equals("no")) {
+                        personaje = "Adam Sandler";
+                    } else {
+                        System.out.println("Respuesta inválida.");
+                    }
                 }
             }else {
                 System.out.println("Respuesta inválida.");
-                personaje = "Personaje no encontrado.";
             }
             scanner.close();
-        } else if (respuesta.equals("ficticio")) {
-            System.out.println("¿A qué categoría pertenece el personaje? (cómic/película/literatura/anime/animación)");
+        } else if (respuesta.equals("no")) {
+            System.out.println("Es un personaje de comic? (Sí/No)");
             respuesta = scanner.nextLine().toLowerCase();
 
-            switch (personaje) {
-                case "comic":
-                    personaje = "Bruce Wayne";
-                    break;
-                case "película":
+            if (respuesta.equals("sí")) {
+                personaje = "Bruce Wayne";
+            } else if (respuesta.equals("no")) {
+                System.out.println("¿Es un personaje de película? (Sí/No)");
+                respuesta = scanner.nextLine().toLowerCase();
+                if (respuesta.equals("sí")) {
                     personaje = "Darth Vader";
-                    break;
-                case "literatura":
-                    personaje = "Tin Tin";
-                    break;
-                case "anime":
-                    personaje = "Goku";
-                    break;
-                case "animación":
-                    personaje = "Ayudante de Santa";
-                    break;
-                default:
+                } else {
                     System.out.println("Respuesta inválida.");
-                    break;
-            } 
+                }
+            }else if(respuesta.equals("no")){
+                System.out.println("¿Es un personaje de anime? (Sí/No)");
+                respuesta = scanner.nextLine().toLowerCase();
+                if (respuesta.equals("sí")) {
+                    personaje = "Goku";
+                } else if (respuesta.equals("no")) {
+                    System.out.println("¿Es un personaje de caricatura? (Sí/No)");
+                    respuesta = scanner.nextLine().toLowerCase();
+                    if (respuesta.equals("sí")) {
+                        personaje = "Tin Tin";
+                    } else if (respuesta.equals("no")) {
+                        personaje = "Ayudante de Santa";
+                    }
+                }
+            }
         } else {
             System.out.println("Respuesta inválida.");
         }
